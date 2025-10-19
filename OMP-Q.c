117748a6fbd49c3027aac0725f1cf7dc165561bf
tjk12 <tjk12@cs-ssh.argo.uwf.edu>
2025-10-19 14:29:37 -0500

    added MT files

---
+++ OMP-Q.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <microtime.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char **argv)
+{
+  
+  int N, i, a, j;
+  float *A=0, x;
+  double t, time1, time2;
+  int NThreads = 1;
+  
+  if(argc != 3)
+  {
+    fprintf(stderr, "USAGE: %s Size NumberOfThreads\n", argv[0]);
+    exit(1);
+  }
+  
+  N = atoi(argv[1]);
+  NThreads = atoi(argv[2]);
+
+  // You must allocate suitably aligned memory unlike this code
+  A = (float *) malloc(N*sizeof(A[0])); 
+  
+  if(A==0)
+    {
+      fprintf(stderr, "Memory allocation failed in file %s, line %d\n", __FILE__, __LINE__);
+      exit(1);
+    }
+
+  time1 = microtime();
+
+  a = 2;
+
+  for(i=0; i < N; i++) 
+    { 
+      a += 2*i;
+      for(j = 0; j < i; j++)
+	{
+	  x = 1.0/(i+j+1.0);
+	  A[i] = x + 1.0;
+	}
+    }
+
+  
+  time2 = microtime();
+  
+  t = time2-time1;
+  printf("\nTime = %g us\tN = %d\tNThreads = %d\n", t, N, NThreads);
+  printf("A[N/2] = %g\ta = %d\n\n", (double) A[N/2], a);
+
+  free(A);
+
+  return 0;
+}
---
+++ madd.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <microtime.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv)
+{
+  
+  int N, i, j;
+  double *A, *B, *C, result=0.0;
+  double time1, time2;
+  
+  if(argc != 2)
+  {
+    fprintf(stderr, "USAGE: %s Matrix-Dimension\n", argv[0]);
+    exit(1);
+  }
+  
+  N = atoi(argv[1]);
+  if(N <= 0 )
+  {
+    fprintf(stderr, "N = %d should be positive\n", N);
+    exit(2);
+  }    
+  
+  A = (void *) malloc(N*N*sizeof(A[0]));
+  B = (void *) malloc(N*N*sizeof(B[0]));
+  C = (void *) malloc(N*N*sizeof(C[0]));
+
+  if(A==0 || B==0 || C==0)
+    {
+      fprintf(stderr, "Memory allocation failed in file %s, line %d\n", __FILE__, __LINE__);
+      exit(1);
+    }
+
+  memset(C, 0, N*N*sizeof(C[0]));
+  
+  for(i=0; i<N; i++)
+    for(j=0; j<N; j++)
+      A[i*N+j] = B[i*N+j] = 0.1;
+  
+  time1 = microtime();
+
+  for(i=0; i<N; i++)
+      for(j=0; j<N; j++)
+	C[i*N+j] += A[i*N+j] + B[i*N+j];
+
+  time2 = microtime();
+  
+  printf("Time = %g us\tTimer Resolution = %g us\tPerformance = %g Gflop/s\n", time2-time1, get_microtime_resolution(), N*N*1e-3/(time2-time1));
+  
+  for(i=0; i<N; i++) /* Check correctness */
+    for(j=0; j<N; j++)
+      result += C[i*N+j];
+  
+  printf("Sum of matrix elements = %g\tExpected value = %g\n", result, (double) 0.2*N*N);
+  
+  return 0;
+}
