2d15ff58f8310bb7ff489dc9a9f27c2235e4058b
Trever Knie <tjk12@students.uwf.edu>
2025-10-19 17:03:10 -0500

    back to dynamic

--- omp.c
+++ omp.c
@@ -42,7 +42,7 @@ int main(int argc, char **argv)
   // Larger chunks distribute work more evenly across threads
   // Chunk size 256: balances overhead reduction with load distribution
   // Still aligns well with cache (256 floats = 1KB, fits in L1)
-  #pragma omp parallel for schedule(static, 256) private(j, x) reduction(+:a)
+  #pragma omp parallel for schedule(dynamic, 256) private(j, x) reduction(+:a)
   for(i=0; i < N; i++) 
   { 
     a += 2*i;
